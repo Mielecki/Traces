c69406e3e9ae653821118a2b5da201891aeb95ce
mielecki <mieleckibartek@gmail.com>
2024-11-20 20:40:22 +0100

    add comments in diekertGraph.go, hasseDiagram.go, change queue implementation

--- internal/graph/diekertGraph.go
+++ internal/graph/diekertGraph.go
@@ -2,10 +2,12 @@ package graph
 
 import "slices"
 
+// Structure to store Diekert graph, created to avoid using functions meant for Diekert graph instead of a normal graph
 type DiekertGraph struct {
 	Graph
 }
 
+// Function to initialize a Diekert Graph for the given word
 func (graph Graph) NewDiekertGraph(wordInput string) DiekertGraph {
 	diekertGraph := DiekertGraph{
 		Graph{
@@ -15,13 +17,15 @@ func (graph Graph) NewDiekertGraph(wordInput string) DiekertGraph {
 	}
 
 	word := []rune(wordInput)
-	vertices := []vertex{}
 
+	// Map storing current number of occurrences of a symbol
 	indices := make(map[rune]int)
 	for v := range graph.adjacencyList {
 		indices[v.name] = -1
 	}
 
+	// Iterate over characters in the word and create a list of vertices with their indices 
+	vertices := []vertex{}
 	for _, c := range word {
 		indices[c] += 1
 		vertices = append(vertices, vertex{
@@ -30,6 +34,7 @@ func (graph Graph) NewDiekertGraph(wordInput string) DiekertGraph {
 		})
 	}
 
+	// Iterate over all possible edges, and if the edge exists in the dependency graph, append it to the Diekert graph
 	for i := 0; i < len(vertices); i++ {
 		v1 := vertices[i]
 		diekertGraph.adjacencyList[v1] = make([]vertex, 0)
--- internal/graph/hasseDiagram.go
+++ internal/graph/hasseDiagram.go
@@ -1,13 +1,16 @@
 package graph
 
 import (
+	"container/list"
 	"sort"
 )
 
+// Structure to store Hasse diagram, created to avoid using functions meant for Hasse diagram instead of a normal graph/Diekert graph
 type HasseDiagram struct {
 	DiekertGraph
 }
 
+// Standard DFS
 func (g *DiekertGraph) dfs(v vertex, visited map[vertex]bool) {
 	visited[v] = true
 	for _, neighbor := range g.adjacencyList[v] {
@@ -17,7 +20,9 @@ func (g *DiekertGraph) dfs(v vertex, visited map[vertex]bool) {
 	}
 }
 
+// Function to initialize a Hasse Diagram
 func (graph *DiekertGraph) NewHasseDiagram() HasseDiagram {
+	// A Hasse Diagram is just a minimized (reduced) Diekert graph
 	reducedGraph := HasseDiagram{
 		DiekertGraph{
 			Graph{
@@ -27,15 +32,17 @@ func (graph *DiekertGraph) NewHasseDiagram() HasseDiagram {
 		},
 	}
 
+	// Copy edges from the Diekert graph to a new graph
 	vertices := []vertex{}
-
 	for k, v := range graph.adjacencyList {
 		reducedGraph.adjacencyList[k] = v
 		vertices = append(vertices, k)
 	}
 
+	// Map storing the set of vertices that are reachable from each vertex
 	reachable := make(map[vertex]map[vertex]bool)
 
+	// For each vertex, use DFS to determine which vertices are reachable
 	for _, u := range vertices {
 		reachable[u] = make(map[vertex]bool)
 
@@ -44,10 +51,11 @@ func (graph *DiekertGraph) NewHasseDiagram() HasseDiagram {
 		}
 	}
 
+	// For each edge u -> v, check if the path u -> w -> v exists. If so, remove the u -> v edge
 	for _, u := range vertices {
 		for _, v := range reducedGraph.adjacencyList[u] {
 			for w := range reachable[u] {
-				if w != u && w != v && reachable[u][v] && reachable[w][v] {
+				if w != u && w != v && reachable[u][w] && reachable[w][v] {
 					reducedGraph.removeEdge(u, v)
 				}
 			}
@@ -57,30 +65,37 @@ func (graph *DiekertGraph) NewHasseDiagram() HasseDiagram {
 	return reducedGraph
 }
 
+// Function to extract Foaty Normal Form from the Hasse diagram using BFS
 func (g *HasseDiagram) GetFNF() string {
+	
+	// BFS counting visit time, but without checking if the vertex has been visited
 	times := make(map[vertex]int)
-	queue := []vertex{}
+	queue := list.New()
 
 	for v := range g.adjacencyList {
-		queue = append(queue, v)
+		queue.PushBack(v)
 		times[v] = 0
 	}
 
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
+	for queue.Len() > 0 {
+		currentElement := queue.Front()
+		queue.Remove(currentElement)
+		current := currentElement.Value.(vertex)
 
 		for _, neighbor := range g.adjacencyList[current] {
 			times[neighbor] = times[current] + 1
-			queue = append(queue, neighbor)
+			queue.PushBack(neighbor)
 		}
 	}
+	// End BFS
 
+	// Grouping vertices by their visit time
 	grouped := make(map[int][]string)
 	for key, value := range times {
 		grouped[value] = append(grouped[value], string(key.name))
 	}
 
+	// Creating a readable output
 	result := ""
 	for i := range len(grouped) {
 		sort.Strings(grouped[i])
